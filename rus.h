40ca2d75158ba90f26f6e39f65e3cb003743c89f
N <62520991+Naster17@users.noreply.github.com>
2025-05-27 21:38:54 +0200

    Add files via upload

---
+++ rus.h
@@ -0,0 +1,78 @@
+#ifndef RUSIFICATOR_H
+#define RUSIFICATOR_H
+
+#define если if
+#define иначе else
+// else if == иначе если
+
+#define пока while
+#define для for
+#define делай do
+
+#define число int
+#define символ char
+#define дробь float
+#define двойной double
+#define короткое short
+#define длиное long
+#define длинный long
+#define знаковый signed
+#define беззнаковый unsigned
+
+#define константа const
+#define констант const
+#define константный const
+#define пустота void
+
+#define и &&
+#define и_равно &=
+#define бит_и &
+#define бит_не |
+#define компл ~
+#define не !
+#define не_равно !=
+#define или ||
+#define или_равно |=
+#define исключ_или ^
+#define исключ_или_равно ^=
+#define равно ==
+#define больше >
+#define меньше <
+
+#define авто auto
+#define выход break
+#define брейк break
+#define выбор switch
+#define вариант case
+#define продолжить continue
+#define продолжать continue
+#define умолчание default
+#define перечисление enum
+#define енумерация enum
+#define внешний extern
+#define внешняя extern
+#define перейти goto
+#define регистер register
+#define размер sizeof
+#define статический static
+#define структура struct
+#define тип_определения typedef
+#define союз union
+#define изменчивый volatile
+#define вернуть return
+#define возврат return
+
+#include <stdio.h>
+#define основной main
+#define печать printf
+#define ввод scanf
+// дальше сами
+
+
+// #define #include #if ...
+// не подерживаются иза специфик языка С
+// но например с англ все работает отлично
+// #define import #include
+// #import <stdio.h>
+
+#endif // !
